a727cce0a20249c7b0ea5bea95d8b90e5affbdc6
Sam2600 <kghtetsan26@gmail.com>
2024-03-25 23:03:47 +0630

    fix path for images

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -7,7 +7,7 @@ export const MealCard = ({ meal }) => {
       <NavLink to={`/meals/${meal.Guid}`}>
         <img
           className="w-full h-60 object-cover p-5"
-          src={`/src/img/${meal.Name}.jpg`}
+          src={`src/img/${meal.Name}.jpg`}
           alt={meal.Name}
         />
         <div className="px-6 pb-4">
